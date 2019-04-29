bcc1606dee9750799ed2d9b4f2b5f4d5862941ea
luca.marzi@gmail.com <luca.marzi@gmail.com>
2019-04-29 17:32:38 +0200

    Initial commit

---
+++ database/migrations/2019_04_29_151418_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 128);
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
